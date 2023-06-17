20b215cf64a13b77bcd94db6412accf7f4b8f78f
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-17 15:12:24 -0300

    background-style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
 export default function Home() {
+
+ 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<h1>Teste</h1>
-    </main>
+    <div className="bg-cover bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+
+    </div>
   )
 }

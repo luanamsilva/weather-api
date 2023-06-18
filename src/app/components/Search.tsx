5819ae88be5cbfda8f6190785a2dfc52a7db5fbb
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-18 17:51:44 -0300

    add-api

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,9 +1,15 @@
 import {BiSearch} from 'react-icons/bi'
-export const Search = ()=>{
+interface InputProps{
+  handleSearch: (e: React.KeyboardEvent<HTMLInputElement>) => void
+  setLocation: React.Dispatch<React.SetStateAction<string>>
+}
+export const Search = ({handleSearch, setLocation}:InputProps)=>{
   return(
     <div className="flex justify-center">
     <form className=' flex w-full items-center justify-center max-w-md bg-white shadow-md rounded px-8 pt-2 pb-2 m-4'>
-    <input  className='rounded-md border-none focus:outline-none text-gray-600' type="text" placeholder='Buscar cidade'/>
+    <input  className='rounded-md border-none focus:outline-none text-gray-600' type="text" placeholder='Buscar cidade'
+    onKeyDown={handleSearch}
+    onChange={(e) => setLocation(e.target.value)}/>
     <BiSearch className='text-gray-600'/>
    </form> 
   </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,42 @@
+"use client"
 import {Search} from './components/Search'
-export default function Home() {
+import React,{ useState } from 'react'
 
+
+export default function Home() {
+const [data, setData] = useState({})
+const [location, setLocation] = useState('')
+const [error, setError] = useState('')
+
+ const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  
+ const handleSearch = async (e: React.KeyboardEvent<HTMLInputElement>)=>{
+  if(e.key === "Enter") {
+    e.preventDefault()
+    try{
+      const response = await fetch(url)
+      if(!response.ok){
+        throw new Error()
+      } const data = await response.json()
+      setData(data)
+      setLocation("")
+      setError("")
+         } catch(error){
+      setError("Cidade não encontrada!")
+      setData({})
+    }
+
+}}
  
-  return (
+ return (
     <div className="bg-cover bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
       
       <div className="rounded-lg flexflex-col h-full">
-       <Search/> 
+     <Search handleSearch={handleSearch} setLocation={setLocation}/> 
+     
       </div>
       
-      
+    
 
     </div>
   )
